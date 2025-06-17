25487de758dbf15a4babea83e753cb0a677082cc
uwizerah <u.kabasinga@gmail.com>
2025-06-17 16:09:04 +0200

    fix: add im local bank transfer

---
+++ kc_custom_app/kc_custom_app/doctype/im_local_bank_transfer/im_local_bank_transfer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kivu Choice and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class IMLocalBankTransfer(Document):
+	pass
